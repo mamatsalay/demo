7254eaba33983301b68f81edbc0efce2e14261f6
mamatsalay <mamatsalaybabadjanov@gmail.com>
2024-08-09 09:13:19 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/dto/IncomeDto.java
@@ -0,0 +1,7 @@
+package com.example.demo.dto;
+
+/**
+ * DTO for {@link com.example.demo.model.Income}
+ */
+public class IncomeDto {
+}
---
+++ src/main/java/com/example/demo/repository/IncomeRepository.java
@@ -0,0 +1,4 @@
+package com.example.demo.repository;
+
+public interface IncomeRepository extends org.springframework.data.jpa.repository.JpaRepository<com.example.demo.model.Income, java.lang.Long> {
+}
